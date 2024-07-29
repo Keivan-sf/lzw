8a3383baa7338b7b950fb6ef01b353325cb89b20
keivan-sf <keyvan0082@gmail.com>
2024-07-29 23:01:29 +0330

    Add symbolEntry struct

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 int main() {
+int number_of_symbols = 0;
+struct symbolEntry {
+  unsigned int symbol;
+  char* ch;
+};
   char ch;
   while((ch = getchar()) != EOF) {
     printf("%c" , ch);
