a8fbdcd9de97768f3a94e1ca96964341870c332d
keivan-sf <keyvan0082@gmail.com>
2024-08-12 18:21:10 +0330

    Add symbol adder

--- main.c
+++ main.c
@@ -28,6 +28,11 @@ int getSymbolNumber(char* ch) {
   return -1;
 }
 
+void addSymbol(char* ch) {
+  symbol_table[number_of_symbols] = ch;
+  number_of_symbols++;
+}
+
 void input() {
   char ch;
   while((ch = getchar()) != EOF) {
@@ -43,6 +48,26 @@ char* concat(const char* str1, const char* str2) {
 }
 
 int main() {
+  for(int i = 0; i < 10; i++) {
+    char b[2] = "";
+    sprintf(b, "%d" , i);
+    char* ch = concat("reset , " , b);
+    addSymbol(ch) ;
+  }
+
+for(unsigned int i = 0; i < 10; i++) {
+    // char * ch = symbol_table[i];
+    char ** ch = malloc(sizeof(char *));
+    int result = 0;
+    if((result = getSymbolValue(i , ch)) >= 0) {
+      printf("%d: %s\n", i , *ch);
+    }
+    free(ch);
+  }
+}
+
+
+void _testfunction() {
   for(int i = 0; i < 10; i++) {
     char b[2] = "";
     sprintf(b, "%d" , i);
@@ -74,38 +99,5 @@ int main() {
   //   }
   //   free(ch);
   // }
-}
 
-
-void _testfunction() {
-  for(int i = 0; i < 10; i++) {
-    char b[2] = "";
-    sprintf(b, "%d" , i);
-    char* ch = concat("reset , " , b);
-    symbol_table[i] = ch;
-    number_of_symbols++;
-  }
-  //
-  // for(unsigned int i = 0; i < 10; i++) {
-  //   struct symbolEntry * se = malloc(sizeof(struct symbolEntry));
-  //   int result = 0;
-  //   if((result = getSymbolViaNumber(i + 256 , se)) > 0) {
-  //     printf("%d: %s\n", se->symbol , se->ch);
-  //   }
-  //   free(se);
-  // }
-
-
-  for(unsigned int i = 0; i < 10; i++) {
-    int result = 0;
-    
-    char b[2] = "";
-    sprintf(b, "%d" , i);
-    char* ch = concat("reset , " , b);
-
-    if((result = getSymbolNumber(ch)) > -1) {
-      printf("%d: %s\n", result , ch);
-    }
-    free(ch);
-  }
 }
