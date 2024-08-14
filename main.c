bcfbaba431a238672591fb9a53ed21c5503ab2dc
keivan-sf <keyvan0082@gmail.com>
2024-08-14 11:03:47 +0330

    Use astyle to format the file

--- main.c
+++ main.c
@@ -6,13 +6,13 @@
 int number_of_symbols = 0;
 struct symbolEntry {
   unsigned int symbol;
-  char* ch;
+  char *ch;
 };
 
-char * symbol_table[1000];
+char *symbol_table[1000];
 
-int getSymbolValue(unsigned int symbol , char ** result) {
-  if(symbol >= number_of_symbols) {
+int getSymbolValue(unsigned int symbol, char **result) {
+  if (symbol >= number_of_symbols) {
     return -1;
   } else {
     *result = symbol_table[symbol];
@@ -20,37 +20,37 @@ int getSymbolValue(unsigned int symbol , char ** result) {
   }
 }
 
-int getSymbolNumber(char* ch) {
-  for(int i = 0; i< number_of_symbols; i++) {
-    if(strcmp(symbol_table[i] , ch) == 0) {
+int getSymbolNumber(char *ch) {
+  for (int i = 0; i < number_of_symbols; i++) {
+    if (strcmp(symbol_table[i], ch) == 0) {
       return i;
     }
   }
   return -1;
 }
 
-void addSymbol(char* ch) {
+void addSymbol(char *ch) {
   symbol_table[number_of_symbols] = ch;
   number_of_symbols++;
 }
 
 void input() {
   char ch;
-  while((ch = getchar()) != EOF) {
-    printf("%c" , ch);
+  while ((ch = getchar()) != EOF) {
+    printf("%c", ch);
   }
 }
 
-char* concat(const char* str1, const char* str2) {
-  char * result = malloc(strlen(str1) + strlen(str2) + 1);
-  strcpy(result , str1);
-  strcat(result , str2);
+char *concat(const char *str1, const char *str2) {
+  char *result = malloc(strlen(str1) + strlen(str2) + 1);
+  strcpy(result, str1);
+  strcat(result, str2);
   return result;
 }
 
 void fillSymbolTableTill256() {
   uint8_t i = 0;
-  for(int j = 0; j < 256; j++) {
+  for (int j = 0; j < 256; j++) {
     char *b = malloc(2 * sizeof(char));
     b[0] = i;
     // printf("We are printing b , %s hello ?\n" , b);
@@ -61,33 +61,32 @@ void fillSymbolTableTill256() {
 
 int main() {
   fillSymbolTableTill256();
-  for(unsigned int j = 0; j < 256; j++) {
-      char ** ch = malloc(sizeof(char *));
-      int result = 0;
-      if((result = getSymbolValue(j , ch)) >= 0) {
-        // printf("%d: %c %02x\n", i , *(*ch+0) , *(*ch+0) & 0xff);
-        printf("%c" , *(*ch+0));
-      }
-      free(ch);
+  for (unsigned int j = 0; j < 256; j++) {
+    char **ch = malloc(sizeof(char *));
+    int result = 0;
+    if ((result = getSymbolValue(j, ch)) >= 0) {
+      // printf("%d: %c %02x\n", i , *(*ch+0) , *(*ch+0) & 0xff);
+      printf("%c", *(*ch + 0));
     }
+    free(ch);
+  }
 }
 
-
 void _testfunction() {
-  for(int i = 0; i < 10; i++) {
+  for (int i = 0; i < 10; i++) {
     char b[2] = "";
-    sprintf(b, "%d" , i);
-    char* ch = concat("reset , " , b);
+    sprintf(b, "%d", i);
+    char *ch = concat("reset , ", b);
     symbol_table[i] = ch;
     number_of_symbols++;
   }
 
-  for(unsigned int i = 0; i < 10; i++) {
+  for (unsigned int i = 0; i < 10; i++) {
     // char * ch = symbol_table[i];
-    char ** ch = malloc(sizeof(char *));
+    char **ch = malloc(sizeof(char *));
     int result = 0;
-    if((result = getSymbolValue(i , ch)) >= 0) {
-      printf("%d: %s\n", i , *ch);
+    if ((result = getSymbolValue(i, ch)) >= 0) {
+      printf("%d: %s\n", i, *ch);
     }
     free(ch);
   }
@@ -95,7 +94,7 @@ void _testfunction() {
   //
   // for(unsigned int i = 0; i < 10; i++) {
   //   int result = 0;
-  //   
+  //
   //   char b[2] = "";
   //   sprintf(b, "%d" , i);
   //   char* ch = concat("reset , " , b);
@@ -105,5 +104,4 @@ void _testfunction() {
   //   }
   //   free(ch);
   // }
-
 }
