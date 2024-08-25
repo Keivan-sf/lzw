c4f04c255049ae83b4329dd5f1b66ec1241d5d8c
keivan-sf <keyvan0082@gmail.com>
2024-08-25 23:32:01 +0330

    Remove size limit for input and output

--- io.c
+++ io.c
@@ -1,12 +1,20 @@
 #include "io.h"
 #include "bits.h"
 #include <stdint.h>
+#include <stdlib.h>
 
-uint8_t output[10000];
+unsigned int outputSize = 0;
+uint8_t *output;
 unsigned int pos = 0;
 void reverseOutputArrayBits();
 
 unsigned int getLen() { return pos / 8 + (pos % 8 > 0); }
+
+void copyUintArrayToAnother(uint8_t *src, uint8_t *dest, int old_len) {
+  for (int i = 0; i < old_len; i++) {
+    dest[i] = src[i];
+  }
+}
 
 void testIO() {
   writeToOutputArray(98, 9);
@@ -18,6 +26,15 @@ void testIO() {
 }
 
 void writeToOutputArray(unsigned int data, unsigned int bits_len) {
+  int neededSize = getLen() + bits_len / 8 + 2;
+  if (outputSize < neededSize) {
+    uint8_t *new_output = malloc(outputSize + 100);
+    copyUintArrayToAnother(output, new_output, outputSize);
+    outputSize += 100;
+    free(output);
+    output = new_output;
+  }
+
   int reversed = reverseBitOrder(data, bits_len);
   pos = writeBitsToUint8Array(reversed, bits_len, pos, output);
 }
