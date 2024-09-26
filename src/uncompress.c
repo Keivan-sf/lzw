5d7012fad1ad4dc831252a27929d190b7758eb85
keivan-sf <keyvan0082@gmail.com>
2024-09-26 13:18:51 +0330

    Fix edge case again

--- src/uncompress.c
+++ src/uncompress.c
@@ -40,6 +40,7 @@ void uncompress() {
   workingData[0] = (*ch)[0];
   workingData[1] = 0;
   printf("%s", *ch);
+  char *prev = *ch;
 
   while (bit_pos + current_margin < number_of_chars * 8) {
     unsigned int reversed_data =
@@ -64,14 +65,23 @@ void uncompress() {
         }
       }
       printf("%s", *ch);
+      prev = *ch;
     } else {
-      char *arg;
-      arg = concatCharToStr(workingData, workingData[0]);
-      addSymbol(arg);
-      char *currentChar = malloc(2 * sizeof(char));
-      currentChar[0] = workingData[0];
-      currentChar[1] = 0;
-      workingData = currentChar;
+      char *current_seq = concatCharToStr(prev, workingData[0]);
+      for (int i = 0; i < strlen(current_seq); i++) {
+        char *arg = concatCharToStr(workingData, current_seq[i]);
+        if (getSymbolNumber(arg) >= 0) {
+          free(workingData);
+          workingData = arg;
+        } else {
+          addSymbol(arg);
+          char *currentChar = malloc(2 * sizeof(char));
+          currentChar[0] = current_seq[i];
+          currentChar[1] = 0;
+          free(workingData);
+          workingData = currentChar;
+        }
+      }
       if (getSymbolValue(data, ch) <= 0) {
         printf("\n\nwe still don't have the symbol bro\n\n");
       } else {
