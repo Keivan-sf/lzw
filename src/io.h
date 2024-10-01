9bbe91a95237a3b8884ad4d24b19fb9b901ddf93
keivan-sf <keyvan0082@gmail.com>
2024-10-01 10:43:24 +0330

    Expose copy uint array util from io

--- src/io.h
+++ src/io.h
@@ -1,5 +1,7 @@
+#include <stdint.h>
 void writeToOutputArray(unsigned int data, unsigned int bits_len);
 void writeOutputArrayToStdOut();
 void writeLZWPrefixToStdOut();
 void initOutput();
 void testIO();
+void copyUintArrayToAnother(uint8_t *src, uint8_t *dest, int old_len);
