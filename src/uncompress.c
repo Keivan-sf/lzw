302c5b52f1fbcdc9c3c021551e83cbc64af4d118
keivan-sf <keyvan0082@gmail.com>
2024-10-01 18:20:21 +0330

    Make readinput void

--- src/uncompress.c
+++ src/uncompress.c
@@ -9,13 +9,13 @@ struct uint8_list {
   unsigned long len, cap;
 };
 
-int readInput(struct uint8_list *input);
+void readInput(struct uint8_list *input);
 
 void uncompress() {
   initiateSymbolTable();
   fillSymbolTableTill256();
   struct uint8_list *input = malloc(sizeof(struct uint8_list));
-  unsigned int number_of_chars = readInput(input);
+  readInput(input);
   for (int i = 0; i < input->len; i++) {
     printf("%u %c -> ", input->data[i], input->data[i]);
     input->data[i] = reverseUint8BitOrder(input->data[i]);
@@ -23,11 +23,10 @@ void uncompress() {
   }
 }
 
-int readInput(struct uint8_list *input) {
+void readInput(struct uint8_list *input) {
   input->data = malloc(sizeof(uint8_t) * 5);
   input->cap = 4;
   input->len = 0;
-  int number_of_chars = 0;
   int iterations = 0;
   char currentByte[1];
   int bytes_read = 0;
@@ -36,7 +35,6 @@ int readInput(struct uint8_list *input) {
     iterations++;
     if (iterations < 4)
       continue;
-    // printf("len: %u, cap %u\n", input->len, input->cap);
     if (input->len + 1 >= input->cap) {
       uint8_t *new_input_data = malloc(sizeof(uint8_t) * (input->cap + 10));
       copyUintArrayToAnother(input->data, new_input_data, input->cap);
@@ -47,6 +45,4 @@ int readInput(struct uint8_list *input) {
     input->data[input->len] = currentByte[0];
     input->len++;
   }
-
-  return number_of_chars;
 }
